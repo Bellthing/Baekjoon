f80099e37ead3f0bf42e0cbc87147ebe292f291e
Bellthing <leejongmyeong7323@gmail.com>
2023-01-12 06:58:09 +0900

    12/01/23

---
+++ 2741.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a, b;
+    b = 1;
+    scanf("%d", &a);
+    while (b<=a){
+        printf("%d\n", b);
+        b++;
+    }
+}
---
+++ 2742.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a, b;
+    scanf("%d", &a);
+    b = a;
+    while (b>=1){
+        printf("%d\n", b);
+        b--;
+    }
+}
